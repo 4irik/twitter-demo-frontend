20bbf9774f59872975020c9036d4c63150114ad8
4irik <chekirilla@gmail.com>
2018-06-19 01:11:28 +0500

    feat: add react-helmet and usee it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Helmet from "react-helmet";
+import logo from "./logo.svg";
+import "./App.css";
+
+const UserProfile = {
+  name: "Every Interaction",
+  account: "EveryInteract"
+};
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
+        <Helmet>
+          <title>
+            {UserProfile.name} (@{UserProfile.account.toLowerCase()}) | Twitter
+          </title>
+        </Helmet>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
